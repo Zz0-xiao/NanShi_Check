f99c45aaa300bf6dd917d9bcb98ebc9b5ee8a7ca
xiao <781962106@qq.com>
2017-02-17 20:56:58 +0800

    刚才坑里，重来

---
+++ app/src/main/java/com/xiao/nanshi_check/activity/WifiTestActivity.java
@@ -0,0 +1,19 @@
+package com.xiao.nanshi_check.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiao.nanshi_check.R;
+
+public class WifiTestActivity extends BaseActivity {
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_wifi_test;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
